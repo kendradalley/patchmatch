b7285ca34958b498a6ed34b41c8012df73dd9d7f
tw3080 <tw3080@gmail.com>
2016-12-10 11:22:24 -0800

    Trying to get ngMaps to work

--- client/js/main_page/controllers/gardenInfoController.js
+++ client/js/main_page/controllers/gardenInfoController.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
-    app.controller('mainController', function() {
+    app.controller('gardenInfoController', function() {
         this.message = 'Hi, you can place the html for the main page in client -> templates -> main_page -> main_directive. Delete this message when ready.';
 
     }
